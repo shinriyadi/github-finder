afcc09ede1fb81b7e9545ec1c377273a179371ad
shinriyadi <shin.riyadi@gmail.com>
2019-08-27 10:27:44 +0700

    Updates

---
+++ precache-manifest.d352a2829d644228849fa9f9a1b15661.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8cecd8650e1d2e4292413b6dabfdf4",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "bb86b868f9ce3d814341",
+    "url": "/github-finder/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "867d480d053366168165",
+    "url": "/github-finder/static/js/2.76e4a6d2.chunk.js"
+  },
+  {
+    "revision": "bb86b868f9ce3d814341",
+    "url": "/github-finder/static/js/main.a83a1dac.chunk.js"
+  },
+  {
+    "revision": "a06ceeb70725f86afdce",
+    "url": "/github-finder/static/js/runtime~main.cb74c248.js"
+  }
+]);
